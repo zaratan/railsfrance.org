86f8e127a98fd5201c663bf5d11f7af16cc0dff6
Pierre Nespo <pierre.nespo@gmail.com>
2013-04-18 18:45:24 +0200

    require uri in the redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,11 @@
 require 'redis'
+require 'uri'
 
 uri = begin
         URI.parse(ENV["REDISTOGO_URL"])
-      rescue
+      rescue => e
+        $stderr.puts e
         Settings.redis
       end
+
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
